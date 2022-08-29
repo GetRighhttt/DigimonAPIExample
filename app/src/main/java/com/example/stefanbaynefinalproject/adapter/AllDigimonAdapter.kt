9102bf6ffb9017c5e51bbc00276d5082d5bbad2d
stefan <stefanbayne@gmail.com>
2022-08-29 14:55:03 -0400

    Changed binding.

--- app/src/main/java/com/example/stefanbaynefinalproject/adapter/AllDigimonAdapter.kt
+++ app/src/main/java/com/example/stefanbaynefinalproject/adapter/AllDigimonAdapter.kt
@@ -8,29 +8,32 @@ import com.example.stefanbaynefinalproject.databinding.AllDigimonListItemBinding
 import com.example.stefanbaynefinalproject.model.Digimon
 
 // Adapter for RecyclerView for API call
-class AllDigimonAdapter(private val digimonListItem: List<Digimon>):
-RecyclerView.Adapter<AllDigimonAdapter.AllDigimonViewHolder>() {
+class AllDigimonAdapter(private val digimonListItem: List<Digimon>) :
+    RecyclerView.Adapter<AllDigimonAdapter.AllDigimonViewHolder>() {
 
     // Inner class for the ViewHolder.
     inner class AllDigimonViewHolder(
         // Create binding value to bind items in layout
         private val binding: AllDigimonListItemBinding
-    ): RecyclerView.ViewHolder(binding.root)
-    {
+    ) : RecyclerView.ViewHolder(binding.root) {
         // Call bind method to bind views to RecyclerView from API
         // Use viewBinding to bind the views, and use Glide to pull the image from the API
         fun bind(digimon: Digimon) {
-            binding.tvName.text = digimon.name.toString()
-            binding.tvLevel.text = digimon.level.toString()
-            Glide.with(binding.ivDigimonItem).load(digimon.img).into(binding.ivDigimonItem)
+            binding.apply {
+                tvName.text = digimon.name.toString()
+                tvLevel.text = digimon.level.toString()
+                Glide.with(ivDigimonItem).load(digimon.img).into(ivDigimonItem)
+            }
         }
     }
 
     // Create ViewHolder, and inflate layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        AllDigimonViewHolder (
-        AllDigimonListItemBinding.inflate(LayoutInflater.from(parent.context),
-        parent, false )
+        AllDigimonViewHolder(
+            AllDigimonListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false
+            )
         )
 
     // Bind ViewHOlder to the position of the listitem
