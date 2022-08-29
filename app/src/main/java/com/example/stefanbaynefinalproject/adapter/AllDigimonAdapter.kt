a28525e0c76b103968bcb551ca43078b05e9e233
stefan <stefanbayne@gmail.com>
2022-08-29 15:30:25 -0400

    Made it more appealing.

--- app/src/main/java/com/example/stefanbaynefinalproject/adapter/AllDigimonAdapter.kt
+++ app/src/main/java/com/example/stefanbaynefinalproject/adapter/AllDigimonAdapter.kt
@@ -2,8 +2,11 @@ package com.example.stefanbaynefinalproject.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.stefanbaynefinalproject.R
 import com.example.stefanbaynefinalproject.databinding.AllDigimonListItemBinding
 import com.example.stefanbaynefinalproject.model.Digimon
 
@@ -12,10 +15,17 @@ class AllDigimonAdapter(private val digimonListItem: List<Digimon>) :
     RecyclerView.Adapter<AllDigimonAdapter.AllDigimonViewHolder>() {
 
     // Inner class for the ViewHolder.
-    inner class AllDigimonViewHolder(
+    class AllDigimonViewHolder(
         // Create binding value to bind items in layout
         private val binding: AllDigimonListItemBinding
     ) : RecyclerView.ViewHolder(binding.root) {
+
+        /*
+        Using our cardview for binding
+         */
+        val cardview: CardView = binding.rvDigiAll
+
+
         // Call bind method to bind views to RecyclerView from API
         // Use viewBinding to bind the views, and use Glide to pull the image from the API
         fun bind(digimon: Digimon) {
@@ -39,6 +49,9 @@ class AllDigimonAdapter(private val digimonListItem: List<Digimon>) :
     // Bind ViewHOlder to the position of the listitem
     override fun onBindViewHolder(holder: AllDigimonViewHolder, position: Int) {
         holder.bind(digimonListItem[position])
+        holder.cardview.startAnimation(
+            AnimationUtils.loadAnimation(holder.itemView.context, R.anim.anim1)
+        )
     }
 
     // Return the item count
